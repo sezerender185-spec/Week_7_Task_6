135ba577d76c98aa665f0280b331c3c81cdf21b9
sezerender185-spec <sezerender185@gmail.com>
2025-11-08 20:26:54 +0300

    Add files via upload

---
+++ Week_7_Task_6.cpp
@@ -0,0 +1,17 @@
+﻿
+#include <iostream>
+bool compare(int x, int y) {   
+        return x<y;
+}
+int main()
+{
+    int a = 10; int b(0);
+
+    bool(*ptr)(int, int);
+    ptr = &compare;
+
+    bool result= compare(a, b);
+    std::cout << result << " Zero is false, One is true!";
+    return 0;
+}
+
